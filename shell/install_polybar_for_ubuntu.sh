24eb4e2a8cd0e0969c205ac0008a5f92ff4fa003
Steve Yu <yugd@xiaopeng.com>
2022-05-20 14:08:46 +0800

    update polybar installation script

--- shell/install_polybar_for_ubuntu.sh
+++ shell/install_polybar_for_ubuntu.sh
@@ -2,7 +2,7 @@
 # https://github.com/cizordj/i3-themer
 
 sudo apt-get install dunst i3 i3lock-fancy polybar xwallpaper rofi bash fonts-font-awesome xdg-utils sensible-utils alsa-utils rxvt-unicode fonts-firacode flameshot xautolock numlockx
-git clone --depth=1 https://github.com/cizordj/i3-themer.git ../apps
+git clone --depth=1 https://github.com/cizordj/i3-themer.git ../apps/i3-themer
 cd ../apps/i3-themer/
 ./i3-themer -l
 ./i3-themer -a 000
